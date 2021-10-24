880d7f995ede8822f10de2233b20ef033a1b9e6f
June24 Wu <932187556@qq.com>
2021-10-24 13:56:45 +0800

    AverageCoin

--- Algorithm/src/Base_Algorithm/ArrangeCoin.java
+++ Algorithm/src/Base_Algorithm/ArrangeCoin.java
@@ -1,2 +1,16 @@
-package Base_Algorithm;public class ArrangeCoin {
+package Base_Algorithm;
+
+public class ArrangeCoin {
+    public static int iteration(int coinNum){
+        var columns = 0;
+        while(true){
+            columns++;
+            if (coinNum >= columns ){
+                coinNum -= columns;
+                continue;
+            }
+            break;
+        }
+        return columns -1 ;
+    }
 }
