101910f51e87836ad669f6232c4e2e875004bd33
June24 Wu <932187556@qq.com>
2021-10-24 13:57:22 +0800

    FindMax

--- Algorithm/src/Base_Algorithm/FindMaxAverage.java
+++ Algorithm/src/Base_Algorithm/FindMaxAverage.java
@@ -1,2 +1,17 @@
-package Base_Algorithm;public class FindMaxAverage {
+package Base_Algorithm;
+
+public class FindMaxAverage {
+    public static float find(int[] array,int num){
+        var point1 = 0;
+        float sum = 0;
+        for (var item = 0 ; item < num;item++){
+            sum += array[item];
+        }
+        for (var point2 = num;point2<array.length;point2++){
+            point1 = point2 - num;
+            if (array[point2] > array[point1])
+                sum = sum +array[point2] - array[point1];
+        }
+        return sum / num;
+    }
 }
