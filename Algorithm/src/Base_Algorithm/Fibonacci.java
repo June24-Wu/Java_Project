3b5040b3d30cd7b4c4bd6bed982f75417ca1f576
June24 Wu <932187556@qq.com>
2021-10-24 13:57:09 +0800

    Fibonacci

--- Algorithm/src/Base_Algorithm/Fibonacci.java
+++ Algorithm/src/Base_Algorithm/Fibonacci.java
@@ -1,2 +1,23 @@
-package Base_Algorithm;public class Fibonacci {
+package Base_Algorithm;
+
+public class Fibonacci {
+    public static int recursion(int index){
+        if (index == 0){return 0;}
+        if (index == 1){return 1;}
+        return recursion(index -1) + recursion(index -2);
+    }
+
+    public static int TwoPointer(int index){
+        if (index == 0){return 0;}
+        else if (index == 1){return 1;}
+        else {
+            int low = 0 , high = 1;
+            for (int i = 2;i<=index;i++){
+                var temp = high;
+                high = low+high;
+                low=temp;
+            }
+            return high;
+        }
+    }
 }
