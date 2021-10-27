87a1c8b56c96f59385ce61b37db041c14e729a55
June24 Wu <932187556@qq.com>
2021-10-27 14:26:42 +0800

    no message

--- Algorithm/src/Base_Algorithm/Main.java
+++ Algorithm/src/Base_Algorithm/Main.java
@@ -9,7 +9,10 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        MaxMoney.get(new int[]{2,1,9,200});
+        var array1 = new int[] {10,24,8,32};
+        var array2 = new int[] {13,25,25,11};
+        System.out.println(Arrays.toString(HorseRacing.get(array2,array1)) );
+
 //        System.out.println(KMP.KMP("ABCBBABBB","BBB"));
 //        System.out.println("Done");
         }
--- Algorithm/src/Base_Algorithm/MaxMoney.java
+++ Algorithm/src/Base_Algorithm/MaxMoney.java
@@ -1,6 +1,8 @@
 package Base_Algorithm;
 
 public class MaxMoney {
+
+//    递归
     public static int get(int[] array){
        return get(array,array.length-1);
     }
@@ -8,5 +10,30 @@ public class MaxMoney {
         if (array ==null || index < 0) return 0;
         if (index ==0) return array[0];
         return Math.max(get(array,index-1) , get(array,index-2) + array[index]);
+    }
+//    动态规划
+    public static  int getLoop(int[] newArray){
+        for (int i = 0; i<newArray.length;i++){
+            if (i>=2)
+                newArray[i] = Math.max(newArray[i] + newArray[i-2],newArray[i-1]);
+            else if (i==1)
+                newArray[i] = Math.max(newArray[0],newArray[1]);
+            else continue;
+        }
+        return newArray[newArray.length-1];
+    }
+
+//    首尾相连
+    public static int getLoop2(int[] array){
+        return Math.max(getLoop2(array,0,array.length-1),getLoop2(array,1,array.length));
+    }
+    private static  int getLoop2(int[] array , int start , int end){
+        int first = array[start] , second = array[start];
+        for (int i = 2; i<end;i++){
+            var temp = second;
+            second = Math.max(array[i] + first, second);
+            first = temp;
+        }
+        return second;
     }
 }
