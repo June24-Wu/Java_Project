abfa9120da8730a32988d1eb39bf8ea3a5d2ff5d
June24 Wu <932187556@qq.com>
2021-10-27 14:27:37 +0800

    no message

---
+++ Upenn_Coursera/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        CustomIntegerArrayList a = new CustomIntegerArrayList();
+        a.add(1);
+        System.out.println(a.getArrayList());
+        System.out.println(a.get(0));
+
+    }
+}
